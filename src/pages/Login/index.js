9608561832641b713de59b73d00671c1dce39aee
Leo Solar <adm@cptsoftware.com>
2021-09-11 18:52:50 -0300

    Criação da Tela de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,51 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';;
+import {styles} from './style';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  Image,
+  StatusBar,
+  Alert,
+  AsyncStorage,
+} from 'react-native';
+
+  export default function Login({}) {
 
 
-function Login() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Login</Text>
-    </View>
-  )
-}
+    const [ email, senha ] = useState('');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ebebeb'
-  },
-  text: {
-    color: '#101010',
-    fontSize: 24,
-    fontWeight: 'bold'
+      return(
+        <View style={styles.container}>
+        <StatusBar translucent hidden />
+  
+        <Image style={styles.logo} source={require('../../assets/logo.png')} />
+  
+        <View style={styles.form}>
+          <TextInput
+            style={styles.login}
+            placeholder="Email"
+            value={email}
+            onChangeText={ (email) => setEmail(email)}
+          />
+  
+          <TextInput
+            secureTextEntry={true}
+            style={styles.login}
+            placeholder="Senha"
+            value={senha}
+            onChangeText={ (senha) => setSenha(senha)}
+          />
+  
+          <TouchableOpacity
+            style={styles.loginSave}
+            onPress={Login}
+          >
+            <Text style={styles.text}>Entrar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+     
+      )
   }
-})
-
-export default Login;
